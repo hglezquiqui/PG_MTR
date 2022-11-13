c2a870b69fae2ceebba2a45c7f21b178f5385861
Odeynis <odeynis.valdes@digitrack.com.br>
2022-11-13 08:21:09 -0500

    -finsih adjust

--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
 import TestingModel as test
 import sys
+from data.RepositoryEnum import RepositoryEnum as RepEnum
 
 if __name__ == '__main__':
     if(sys.argv[1] == 'MTR_EA'):
         test.MTR_EA()
     elif(sys.argv[1] == 'GMLR1'):
-        dataset = {'name': [sys.argv[2]], 'output': [2]}
+        dataset = {'name': [sys.argv[2]], 'output': [RepEnum[sys.argv[2]].value]}
         test.GMLR1(dataset)
